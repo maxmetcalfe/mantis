7353ecb0bef478f1c7ad5a7a31387c702f0b4372
Max Metcalfe <m.maxmetcalfe@gmail.com>
2016-03-19 16:59:24 -0700

    Add virtual Python display

--- UltraSignup/entrants.py
+++ UltraSignup/entrants.py
@@ -1,4 +1,12 @@
+# Python script to print the number of race entrants from Ultra Signup events.
+# Max Metcalfe - 3/19/2016
+
+from pyvirtualdisplay import Display
 from selenium import webdriver
+
+# Create fake display
+display = Display(visible=0, size=(1024, 768))
+display.start()
 
 # Quicksilver 100K 2016
 driver = webdriver.Firefox()
@@ -13,3 +21,4 @@ driver.get("https://ultrasignup.com/entrants_event.aspx?did=36634")
 entrant_element = driver.find_element_by_id('ContentPlaceHolder1_lblCount')
 print "Pine to Palm 100: " + entrant_element.text
 driver.close()
+display.stop()
