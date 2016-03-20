f6758122d02a92c8f4849c91329ee8a707065417
Max Metcalfe <m.maxmetcalfe@gmail.com>
2016-03-19 20:45:03 -0700

    Adding email config in entrants.py

--- UltraSignup/entrants.py
+++ UltraSignup/entrants.py
@@ -3,6 +3,23 @@
 
 from pyvirtualdisplay import Display
 from selenium import webdriver
+import argparse
+import smtplib
+
+parser = argparse.ArgumentParser()
+
+# To Do - make this secure :)
+parser.add_argument( "-u", "--user", help="gmail username", required=True )
+parser.add_argument( "-p", "--password", help="gmail password", required=True  )
+args = parser.parse_args()
+username = args.user
+password = args.password
+
+# Email settings
+fromaddr = args.username
+toaddrs  = 'm.maxmetcalfe@gmail.com'
+msg = ""
+subject = "Ultra Signup Update"
 
 # Quicksilver 100K 2016
 display = Display(visible=0, size=(1024, 768))
@@ -10,7 +27,7 @@ display.start()
 driver = webdriver.Firefox()
 driver.get("https://ultrasignup.com/entrants_event.aspx?did=34516")
 entrant_element = driver.find_element_by_id('ContentPlaceHolder1_lblCount')
-print "Quicksilver 100K: " + entrant_element.text
+msg = msg + "\n" + "QuickSilver 100K: " + entrant_element.text
 driver.close()
 display.stop()
 
@@ -20,6 +37,21 @@ display.start()
 driver = webdriver.Firefox()
 driver.get("https://ultrasignup.com/entrants_event.aspx?did=36634")
 entrant_element = driver.find_element_by_id('ContentPlaceHolder1_lblCount')
-print "Pine to Palm 100: " + entrant_element.text
+msg = msg + "\n" + "Pine to Palm 100: " + entrant_element.text
 driver.close()
 display.stop()
+
+# Prepare email
+msg = """\
+From: %s
+To: %s
+Subject: %s
+
+%s
+""" % (fromaddr, toaddrs, subject, msg)
+
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.starttls()
+server.login(args.user,args.password)
+server.sendmail(fromaddr, toaddrs, msg)
+server.quit()
